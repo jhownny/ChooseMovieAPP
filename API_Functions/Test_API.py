aeb37e91269ffe4cd5afa74c9683942d00e06907
Jhonata Batista <jho.bat9@gmail.com>
2023-10-16 00:13:48 -0300

    Implement of GUI end tests - 16/10

--- API_Functions/Test_API.py
+++ API_Functions/Test_API.py
@@ -1,11 +1,12 @@
 import requests
-from keypath import chave_API
-#api_key={chave_API}
-url = f"https://api.themoviedb.org/3/genre/movie/list?primary_release_year=1980&language=enapi_key={chave_API}"
+from keypath import *
+# api_key={chave_API}
+
+url = f"https://api.themoviedb.org/3/genre/movie/list?primary_release_year=1980&language=en&api_key={chave_API}"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjQ3NmRhNGZhYWJkNWYwNzRiYTA4ODVkYjRhMWIzMCIsInN1YiI6IjY1MmFmYTBkZWE4NGM3MDEwYzFiYWY4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4ghy1utaf22jXRsorcEp5N269QwYPa7muL_e_X8YzS4"
+    "Authorization": f"{Code}"
 }
 
 response = requests.get(url, headers=headers)
