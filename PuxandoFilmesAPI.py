382087dead2655d7fd480be1a2e06b5161183952
Jhonata Batista <jho.bat9@gmail.com>
2023-10-17 01:17:57 -0300

    learning from mistakes and raising the level of my code - 17/10

--- PuxandoFilmesAPI.py
+++ PuxandoFilmesAPI.py
@@ -14,7 +14,7 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-
+a = "chuleta"
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
@@ -28,20 +28,37 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        self.tabview = customtkinter.CTkTabview(self, width=50)
-        self.tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="n")
-        self.tabview.add("CTkTabview")
-        self.tabview.add("Tab 2")
-        self.tabview.add("Tab 3")
-        self.tabview.tab("CTkTabview").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
-        self.tabview.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        self.tabview = customtkinter.CTkTabview(self, width=550)
+        self.tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="ns")
+        self.tabview.add("ChooseMovie")
+        self.tabview.tab("ChooseMovie").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
 
-        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("CTkTabview"), dynamic_resizing=False,
-                                                        values=["Aventura", "Terror", "Ação", "Suspense"])
-        self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.combobox_1 = customtkinter.CTkComboBox(self.tabview.tab("CTkTabview"),
-                                                    values=["Value 1", "Value 2", "Value Long....."])
-        self.combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
+        self.optionmenu_1 = customtkinter.CTkOptionMenu(self.tabview.tab("ChooseMovie"), dynamic_resizing=False,
+                                                        values=["Aleatorio", "Aventura", "Terror", "Ação", "Suspense"])
+        self.optionmenu_1.grid(row=0, column=1, padx=20, pady=(20, 10))
+
+        self.label = customtkinter.CTkLabel(self.tabview.tab("ChooseMovie"), text="...", fg_color="transparent")
+        self.label.grid(row=2, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
+
+        self.entry = customtkinter.CTkEntry(self.tabview.tab("ChooseMovie"), placeholder_text="Digite o Ano do Filme")
+        self.entry.grid(row=1, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
+
+        self.RandomButton = customtkinter.CTkButton(self.tabview.tab("ChooseMovie"), text="Aleatorizar", fg_color="transparent", border_width=2,
+                                                     text_color=("gray10", "#DCE4EE"), command=self.change_label_information)
+        self.RandomButton.grid(row=3, column=0, padx=(20, 20), pady=(150, 20), sticky="sw")
+
+        self.ExitButton = customtkinter.CTkButton(master=self.tabview.tab("ChooseMovie"), text="Exit", fg_color="transparent", border_width=2,
+                                                     text_color=("gray10", "#DCE4EE"))
+        self.ExitButton.grid(row=3, column=2, padx=(20, 20), pady=(150, 20), sticky="se")
+
+        # self.string_input_button = customtkinter.CTkButton(self.tabview.tab("ChooseMovie"), text="Open CTkInputDialog",
+        #                                                    command=self.open_input_dialog_event)
+        # self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
+
+    def change_label_information(self):
+        label = customtkinter.CTkLabel(self.tabview.tab("ChooseMovie"), text=(f"{a}"), fg_color="transparent")
+        label.grid(row=2, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
+
 
 if __name__ == "__main__":
     app = App()
