13ed5296cedc79a21cd311b442b021879c37b254
Jhonata Batista <jho.bat9@gmail.com>
2023-10-21 03:06:28 -0300

    add Folder - 21/10

---
+++ API_Functions/__init__.py
@@ -0,0 +1,27 @@
+import API_Settings.keypath
+from API_Settings.Test_API import *
+import requests
+
+def API_Movie():
+
+    url = f"{API_Settings.keypath.siteURL}/genre/movie/list?&language=pt&api_key={API_Settings.keypath.chave_API}"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"{API_Settings.keypath.Code}"
+    }
+
+    response = requests.get(url)
+    response = response.json()
+    response_name = response['genres']
+    outnumber = 0
+    ListGener = ['Aleatorio']
+    length = len(response_name)
+
+    while True:
+        if outnumber < length:
+            FirstStep = response_name[outnumber]
+            ListGener.append(FirstStep['name'])
+            outnumber += 1
+        else:
+            return ListGener
+            break
