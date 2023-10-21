31c23a7989493c8f045288134b38cbba94f49cd4
Jhonata Batista <jho.bat9@gmail.com>
2023-10-21 04:35:26 -0300

    Fixing Errors - 21/10

--- PuxandoFilmesAPI.py
+++ PuxandoFilmesAPI.py
@@ -9,57 +9,78 @@
 
 
 from API_Functions import *
-import tkinter
-import tkinter.messagebox
-import customtkinter
+from customtkinter import *
 
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
+set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-class App(customtkinter.CTk):
+class Validadores:
+    def validade_entry(self, text):
+        if text == "": return True
+        try:
+            value = int(text)
+        except ValueError:
+            return False
+        return 0 <= value <= 100
+
+
+class App(CTk, Validadores):
+
     def __init__(self):
         super().__init__()
 
         # configure window
         self.title("CustomTkinter complex_example.py")
         self.geometry(f"{1100}x{580}")
+        self.validaEntradas()
 
         # configure grid layout (4x4)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        self.tabview = customtkinter.CTkTabview(self, width=550)
+        self.tabview = CTkTabview(self, width=550)
         self.tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="ns")
         self.tabview.add("ChooseMovie")
         self.tabview.tab("ChooseMovie").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
 
-        self.optionmenu = customtkinter.CTkOptionMenu(self.tabview.tab("ChooseMovie"), dynamic_resizing=False,
+        self.optionmenu = CTkOptionMenu(self.tabview.tab("ChooseMovie"), dynamic_resizing=False,
                                                         values=API_Movie())
         self.optionmenu.grid(row=0, column=1, padx=20, pady=(20, 10))
 
-        self.label = customtkinter.CTkLabel(self.tabview.tab("ChooseMovie"), text="...", fg_color="transparent")
+        self.label = CTkLabel(self.tabview.tab("ChooseMovie"), text="...", fg_color="transparent")
         self.label.grid(row=2, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
 
-        self.entry = customtkinter.CTkEntry(self.tabview.tab("ChooseMovie"), placeholder_text="Digite o Ano do Filme")
+        self.entry = CTkEntry(self.tabview.tab("ChooseMovie"), validate="key", validatecommand=self.vcmd)
         self.entry.grid(row=1, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
 
-        self.RandomButton = customtkinter.CTkButton(self.tabview.tab("ChooseMovie"), text="Aleatorizar", fg_color="transparent", border_width=2,
-                                                     text_color=("gray10", "#DCE4EE"), command=self.change_label_information)
+        self.RandomButton = CTkButton(self.tabview.tab("ChooseMovie"), text="Aleatorizar", fg_color="transparent",
+                                      border_width=2,
+                                      text_color=("gray10", "#DCE4EE"), command=self.change_label_information)
+
         self.RandomButton.grid(row=3, column=0, padx=(20, 20), pady=(150, 20), sticky="sw")
 
-        self.ExitButton = customtkinter.CTkButton(master=self.tabview.tab("ChooseMovie"), text="Sair", fg_color="transparent", border_width=2,
+        self.ExitButton = CTkButton(master=self.tabview.tab("ChooseMovie"), text="Sair", fg_color="transparent", border_width=2,
                                                      text_color=("gray10", "#DCE4EE"), command=self.destroy)
         self.ExitButton.grid(row=3, column=2, padx=(20, 20), pady=(150, 20), sticky="se")
 
-        # self.string_input_button = customtkinter.CTkButton(self.tabview.tab("ChooseMovie"), text="Open CTkInputDialog",
+        # self.string_input_button = CTkButton(self.tabview.tab("ChooseMovie"), text="Open CTkInputDialog",
         #                                                    command=self.open_input_dialog_event)
         # self.string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
 
     def change_label_information(self):
-        label = customtkinter.CTkLabel(self.tabview.tab("ChooseMovie"), text=("vá toma no cu"), fg_color="transparent")
+
+        label = CTkLabel(self.tabview.tab("ChooseMovie"), text="vá toma no cu", fg_color="transparent")
         label.grid(row=2, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
+
+    def validaEntradas(self):
+
+        self.vcmd = (self.register(self.validade_entry), '%p')
+
+    def entrythings(self):
+
+        valuething = int(self.entry.get())
 
 
 if __name__ == "__main__":
