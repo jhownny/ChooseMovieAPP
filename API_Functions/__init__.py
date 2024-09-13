7b5a27e9d4d133bc1a753963e675d095d6676a8f
Jhownny <jho.bat9@gmail.com>
2024-09-13 17:19:43 -0300

    Make Fixing and Testing more codes - 13/09/24 Desktop

--- API_Functions/__init__.py
+++ API_Functions/__init__.py
@@ -1,28 +1,32 @@
+import requests
 import API_Settings.keypath
 from API_Settings.keypath import *
-import requests
+from datetime import datetime
 
-def API_Movie():
-
+def API_GenerMovie():
     url = f"{API_Settings.keypath.webURL}/genre/movie/list?&language=pt-BR&api_key={API_Settings.keypath.key_API}"
     headers = {
         "accept": "application/json",
         "Authorization": f"{API_Settings.keypath.Code}"
     }
 
-    response = requests.get(url)
-    response = response.json()
+    response = requests.get(url).json()
+    genres = response['genres']
+    genre_list = ['Movies']
 
-    response_name = response['genres']
-    outnumber = 0
-    ListGener = ['Aleatorio']
-    length = len(response_name)
+    for genre in genres:
+        genre_list.append(genre['name'])
+
+    return genre_list
+
+def API_YearMovie():
+    current_year = datetime.now().year
+    year_list = []
+    start_year = 1985
 
     while True:
-        if outnumber < length:
-            FirstStep = response_name[outnumber]
-            ListGener.append(FirstStep['name'])
-            outnumber += 1
+        if len(year_list) < (current_year - start_year + 1):
+            year_list.append(str(start_year + len(year_list)))
         else:
-            return ListGener
+            return year_list
             break
--- PuxandoFilmesAPI.py
+++ PuxandoFilmesAPI.py
@@ -8,11 +8,12 @@
 # - present the name and exactly year information of the random movie of system choose;
 
 import PuxandoFilmesAPI
+from PIL import Image
 from API_Functions import *
 from customtkinter import *
 from API_Functions.SearchingMovie import *
 
-set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
 set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 class Validadores:
@@ -34,14 +35,69 @@ class App(CTk, Validadores):
         self.geometry(f"{1100}x{580}")
         self.validaEntradas()
 
-        # configure grid layout (4x4)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
+        # Configure Grid Layout (5x5)
+        '''
+        #Column Configuration: Position 0 - width 0 / Position 1 - width 1 / Position 2 - width 0
+        self.grid_columnconfigure((0,1,2,3,4), weight=0)
+        #Row Configuration: Position 0, 1 and 2 - width 1
+        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        '''
+
+        self.grid_columnconfigure(3, weight=1)
+        self.grid_columnconfigure((1,2), weight=0)
+        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
+
+        # Image Configuration
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "APP_Functions/Images")
+        self.logo_image = CTkImage(Image.open(os.path.join(image_path, "Logo.png")),
+                                                 size=(30, 30))
+
+        # Configure Left Side Bar
+        self.Sidebar_frame = CTkFrame(self, corner_radius=0)
+        self.Sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="NSEW")
+        self.Sidebar_frame.grid_rowconfigure(4, weight=1)
+
+        self.Sidebar_frame_label = CTkLabel(self.Sidebar_frame, text="  ChooseMovie",
+                                                             image=self.logo_image,
+                                                             compound="left",
+                                                             font=CTkFont(size=15, weight="bold"))
+        self.Sidebar_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        # Gener of Movies Dropdown Configurations
+        self.MovieOptionMenu = CTkOptionMenu(self, dynamic_resizing=False, values=API_GenerMovie())
+        self.MovieOptionMenu.grid(row=1, column=0, padx=20, pady=(20, 10))
+
+        # Year Dropdown Configuration
+        self.YearOptionMenu = CTkOptionMenu(self, dynamic_resizing=True, values=API_YearMovie())
+        self.YearOptionMenu.grid(row=2, column=0, padx=20, pady=(20, 10))
+
+        # Random Button Configuration
+        self.RandomButton = CTkButton(self, text="Randomize", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"))
+        self.RandomButton.grid(row=4, column=1, padx=(20, 20), pady=(20,20), sticky="NSEW")
+
+        # Exit Button Configuration
+        self.ExitButton = CTkButton(master=self, text="Exit", fg_color="transparent", border_width=2, text_color=("gray10", "#DCE4EE"), command=self.destroy)
+        self.ExitButton.grid(row=4, column=2, padx=(20, 20), pady=(20, 20), sticky="NSEW")
 
         # configure "ChooseMovie" tab
-        self.tabview = CTkTabview(self, width=550)
-        self.tabview.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="ns")
+        self.MovieCoverFrame = CTkTabview(self, width=650)
+        self.MovieCoverFrame.grid(row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.MovieCoverFrame.add("ChooseMovie")
+        self.MovieCoverFrame.tab("ChooseMovie").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+
+        self.TitleLabel = CTkLabel(self.MovieCoverFrame.tab("ChooseMovie"),text="Title", compound="bottom", font=CTkFont(size=15, weight="bold"))
+        self.TitleLabel.grid(row=1, column=1, padx=20, pady=20)
+
+        '''
+        self.Sidebar_logo = CTkImage(light_image=Image.open("APP_Functions/Images/Logo.png"), dark_image=Image.open("APP_Functions/Images/Logo.png") , size=(100, 100))
+
+        # Use 'self' no lugar de 'app'
+        self.Image_label = CTkLabel(self, image=self.Sidebar_logo, text="")  # display image with a CTkLabel
+
+        
+        # configure "ChooseMovie" tab
+        self.tabview = CTkTabview(self, width=650)
+        self.tabview.grid(row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="ns")
         self.tabview.add("ChooseMovie")
         self.tabview.tab("ChooseMovie").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
 
@@ -49,8 +105,11 @@ class App(CTk, Validadores):
         self.optionmenu = CTkOptionMenu(self.tabview.tab("ChooseMovie"), dynamic_resizing=False, values=API_Movie())
         self.optionmenu.grid(row=0, column=1, padx=20, pady=(20, 10))
 
-        self.label = CTkLabel(self.tabview.tab("ChooseMovie"), text="...", fg_color="transparent")
-        self.label.grid(row=2, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
+        self.MovieLabel = CTkLabel(self.tabview.tab("ChooseMovie"), text="...", fg_color="transparent")
+        self.MovieLabel.grid(row=2, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
+
+        self.YearLabel = CTkLabel(self.tabview.tab("ChooseMovie"), text="Ano", fg_color="transparent")
+        self.YearLabel.grid(row=3, column=1, padx=(5, 0), pady=(20, 10), sticky="ns")
 
         self.RandomButton = CTkButton(self.tabview.tab("ChooseMovie"), text="Aleatorizar", fg_color="transparent", border_width=2,
                                       text_color=("gray10", "#DCE4EE"), command=self.change_label_information)
@@ -66,16 +125,22 @@ class App(CTk, Validadores):
 
         self.year = None
 
+        # create radiobutton frame
+        self.radiobutton_frame = CTkFrame(self.tabview.tab("ChooseMovie"), border_color = "green", border_width = 3)
+        self.radiobutton_frame.grid(row=2, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew")
+        self.radio_var = IntVar(value=0)
+        self.label_radio_group = CTkLabel(master=self.radiobutton_frame, text="CTkRadioButton Group:")
+        
     def change_label_information(self):
-        self.label.configure(text="...")  # Clear the label first
+        self.MovieLabel.configure(text="...")  # Clear the label first
         genre = self.optionmenu.get()
-        self.label.configure(text=f"Você selecionou: {genre}")
+        self.MovieLabel.configure(text=f"Você selecionou: {genre}")
 
     def open_input_dialog_event(self):
         dialog = CTkInputDialog(text="Escolha o ano do filme:", title="Ano do Filme")
         self.year = dialog.get_input()
-        self.label.configure(text=f"Ano selecionado: {self.year}")
-
+        self.YearLabel.configure(text=f"Ano selecionado: {self.year}")
+    '''
     def validaEntradas(self):
         self.vcmd = (self.register(self.validade_entry), '%p')
 
